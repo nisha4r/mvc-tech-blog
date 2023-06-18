fe3d2574954bc25418114c2c8aa6ebd077a9b16b
nisha4r <nisha.4r@gmail.com>
2023-06-18 15:13:02 -0600

    Adding all required routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
