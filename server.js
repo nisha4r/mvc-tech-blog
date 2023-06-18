16a49256c180262e308f0de3c3f5b04e336b23f1
nisha4r <nisha.4r@gmail.com>
2023-06-18 14:38:16 -0600

    fixing handlebar

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ const sess = {
     db: sequelize
   })
 };
-
-app.engine('handlebars', exphbs.engine);
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
