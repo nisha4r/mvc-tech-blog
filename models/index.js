f63e73c855dbf3091239c9c4a2a5a354397dad6a
nisha4r <nisha.4r@gmail.com>
2023-06-18 15:24:14 -0600

    update index

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./User');
-const Post = require('./BlogPost');
+const BlogPost = require('./BlogPost');
 const Comment = require('./Comment');
+User.hasMany(Comment, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(BlogPost, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
