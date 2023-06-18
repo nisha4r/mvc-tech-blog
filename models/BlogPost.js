b397f21850a5ee98969fc99524b0b9a8f8ca3125
nisha4r <nisha.4r@gmail.com>
2023-06-18 15:21:01 -0600

    Adding blogpost model

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,6 +1,37 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class BlogPost extends Model {}
-
+class BlogPost extends Model { }
+BlogPost.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blogpost',
+    }
+);
 module.exports = BlogPost;
