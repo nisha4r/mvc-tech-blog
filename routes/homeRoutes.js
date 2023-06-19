36fecf96ec439e783bf12ac30510ca5fe5398bd0
nisha4r <nisha.4r@gmail.com>
2023-06-18 18:26:32 -0600

    Adding blogpost data

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,30 +1,52 @@
 const router = require('express').Router();
-const {User } = require('../models/User');
+const { User } = require('../models/User');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  try {
-   
-    // Pass serialized data and session flag into template
-    res.render('homepage', {
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    try {
+        const blogPostData = await Post.findAll({
+            attributes: ['id', 'title', 'content', 'created_at'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment', 'blogPostId', 'userId', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+            order: [['created_at', 'DESC']],
+        })
+
+        const blogPosts = blogPostData.map((post) => post.get({ plain: true }));
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            blogPosts,
+            logged_in: req.session.logged_in,
+            username: req.session.username,
+            userId: req.session.userId
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 
 
 
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
 
-  res.render('login');
+    res.render('login');
 });
 
 module.exports = router;
