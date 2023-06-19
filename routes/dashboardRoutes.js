b5aec341b0c4ed39008bd1d939af13458c6a5457
nisha4r <nisha.4r@gmail.com>
2023-06-19 00:34:57 -0600

    update dashboard routes

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -2,3 +2,15 @@ const router = require('express').Router();
 const { User, BlogPost, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 module.exports = router;
+
+router.get('/', withAuth, (req, res) => {
+    BlogPost.findAll({});
+
+});
+router.get('/create', withAuth, (req, res) => {
+    res.render('createpost', { username: req.session.username });
+});
+router.get('/update/:id', withAuth, (req, res) => {
+    BlogPost.findOne({});
+});
+
