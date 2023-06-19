d160d9ee4b35276ad779adebd97903714c1e31ee
nisha4r <nisha.4r@gmail.com>
2023-06-19 00:41:48 -0600

    update dashboard route

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -4,13 +4,35 @@ const withAuth = require('../utils/auth');
 module.exports = router;
 
 router.get('/', withAuth, (req, res) => {
-    BlogPost.findAll({});
-
+    BlogPost.findAll({}).then((data) => {
+        if (!data) {
+            res.status(404).json({ message: 'Get All Blog Post Failed' });
+            return;
+        }
+        const blogpost = data.get({ plain: true });
+        res.render('dashboard', { blogpost, loggedIn: true, username: req.session.username });
+    })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 router.get('/create', withAuth, (req, res) => {
-    res.render('createpost', { username: req.session.username });
+    res.render('createblog', { username: req.session.username });
 });
 router.get('/update/:id', withAuth, (req, res) => {
-    BlogPost.findOne({});
+    BlogPost.findOne({})
+        .then((updateData) => {
+            if (!updateData) {
+                res.status(404).json({ message: 'Update Post Failed' });
+                return;
+            }
+            const blogpost = updateData.get({ plain: true });
+            res.render('updateblog', { blogpost, loggedIn: true, username: req.session.username });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
