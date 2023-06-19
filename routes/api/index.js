8114835af34ecd36d1f54d8d1535ff8de0520a30
nisha4r <nisha.4r@gmail.com>
2023-06-18 19:02:23 -0600

    Add login and post , comment routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-
+const blogPostRoutes = require('./postRoutes');
 const userRoutes = require('./userRoutes');
-
+const commentRoutes = require('./commentRoutes');
 router.use('/users', userRoutes);
+router.use('/blogpost', blogPostRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,6 +3,20 @@ const bcrypt = require('bcrypt');
 const User = require('../../models/User');
 const { error } = require('console');
 
+router.post('/', async (req, res) => {
+    try {
+      const userData = await User.create(req.body);
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+  
+        res.status(200).json(userData);
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 // post login api to verify username and password
 router.post('/login', async (req, res) => {
     try {
@@ -32,5 +46,16 @@ router.post('/login', async (req, res) => {
 
 
 });
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+  
 
 module.exports = router;
