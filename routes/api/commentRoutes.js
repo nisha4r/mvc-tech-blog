50bfcc96fd6bba8e96581bfdd4d43ec267538a92
nisha4r <nisha.4r@gmail.com>
2023-06-19 23:25:27 -0600

    update comment route

--- routes/api/commentRoutes.js
+++ routes/api/commentRoutes.js
@@ -3,14 +3,23 @@ const { Comment } = require('../../models/Comment');
 const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
-
+    try {
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/:id', async (req, res) => {
-
+    try {
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.post('/', withAuth, async (req, res) => {
-
+    try {
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 module.exports = router;
