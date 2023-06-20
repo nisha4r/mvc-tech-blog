496af5d1babd4289cc81e2c52d0c9f70e943d90c
nisha4r <nisha.4r@gmail.com>
2023-06-19 23:23:54 -0600

    comment routes

--- routes/api/commentRoutes.js
+++ routes/api/commentRoutes.js
@@ -1,2 +1,16 @@
 const router = require('express').Router();
+const { Comment } = require('../../models/Comment');
+const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+
+});
+
+router.get('/:id', async (req, res) => {
+
+});
+
+router.post('/', withAuth, async (req, res) => {
+
+});
 module.exports = router;
