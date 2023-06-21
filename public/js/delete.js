dca53996043b815c62dc6f55ecdfd2e74d547235
nisha4r <nisha.4r@gmail.com>
2023-06-21 08:21:12 -0600

    update delete post

--- public/js/delete.js
+++ public/js/delete.js
@@ -2,7 +2,15 @@ const blogPostId = document.querySelector('#blogpostid').value;
 const deleteButton = document.querySelector('#delete-btn');
 
 const deleteButtonHandler = async() =>{
-
+    const response = await fetch(`/api/blogpost/${blogPostId}`, {
+        method: 'DELETE'
+    });
+    if (response.ok) {
+    document.location.replace('/dashboard');
+    } else {
+        alert(response.status);
+    }
 }
 
+if(deleteButton != null)
 deleteButton.addEventListener('click', deleteButtonHandler);
